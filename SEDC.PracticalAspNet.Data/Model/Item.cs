d18964c8c828230a3713c8267c154825f571de7b
Dzevat Ibraimi <Dzevat.Ibraimi@seavus.com>
2018-09-03 20:34:21 +0200

    added rest of the projects

---
+++ SEDC.PracticalAspNet.Data/Model/Item.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SEDC.PracticalAspNet.Data.Model
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        [MaxLength(2500)]
+        public string Contents { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public bool Availability { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
