5c826bfa81f93c7d0884c751b232ac1c4f5994d9
Dzevat Ibraimi <Dzevat.Ibraimi@seavus.com>
2018-09-12 18:48:20 +0200

    added new webdemo

---
+++ SEDC.PractialAspNet.WebDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SEDC.PractialAspNet.WebDemo.MvcApplication" Language="C#" %>
---
+++ SEDC.PractialAspNet.WebDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SEDC.PractialAspNet.WebDemo.Startup))]
+namespace SEDC.PractialAspNet.WebDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
