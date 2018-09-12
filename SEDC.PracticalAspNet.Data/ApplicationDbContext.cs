6fd56b14875e649e63c942936a62387aac5a0cfb
Dzevat Ibraimi <Dzevat.Ibraimi@seavus.com>
2018-09-12 19:20:04 +0200

    moved identity to a separate project

---
+++ SEDC.PracticalAspNet.Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace SEDC.PractialAspNet.WebDemo.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+            
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
