edfcc5ae9836f5bb18f76ca26b5638d5982f9e85
Dzevat Ibraimi <Dzevat.Ibraimi@seavus.com>
2018-09-12 19:24:06 +0200

    fixed references

--- SEDC.PracticalAspNet.Data/ApplicationUser.cs
+++ SEDC.PracticalAspNet.Data/ApplicationUser.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
